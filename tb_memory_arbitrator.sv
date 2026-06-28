dfd184b93c15cf59c6b89cbf0eaaeb3708fbc2ae
Om <omgupta2806@gmail.com>
2026-06-28 19:08:37 +0530

    broadcasting and conflict in same bank test

--- tb_memory_arbitrator.sv
+++ tb_memory_arbitrator.sv
@@ -3,16 +3,21 @@ module tb_memory_arbitrator;
     parameter DW         = 16;
     parameter NUMBER_OF_THREADS  = 16; //systolic grid size 
     parameter ADDR_DEPTH = 16;
+    parameter NUMBER_OF_WARPS = 4;
 
     logic clk;
     logic reset; 
     logic matmul;
     logic mem_write;
     logic mem_req;
-    logic[ADDR_DEPTH - 1 : 0] addr[0 : NUMBER_OF_THREADS - 1]; // receive te whole address ad decode it here 
-    logic[DW - 1 : 0]        data_in[0 : NUMBER_OF_THREADS - 1];
-    logic[DW -  1 : 0]       data_out[0 : NUMBER_OF_THREADS - 1];    
-    logic[NUMBER_OF_THREADS - 1 : 0] active_mask;
+    logic request_type;
+    logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]warp_id_from_ws;
+    logic[ADDR_DEPTH - 1 : 0]              addr[0 : NUMBER_OF_THREADS - 1]; // receive te whole address ad decode it here 
+    logic[DW - 1 : 0]                      data_in[0 : NUMBER_OF_THREADS - 1];
+    logic[DW -  1 : 0]                     data_out[0 : NUMBER_OF_THREADS - 1];  
+    logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]rf_to_access;
+    logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]warp_id_to_ws;  
+    logic[NUMBER_OF_THREADS - 1 : 0]       active_mask;
     logic stall;
 
     arbitrator dut (.*);
@@ -119,14 +124,6 @@ end
         repeat(17) @(posedge clk);
 
 
-        // for (integer i = 0; i < 16; i++) begin
-        //     if (data_out[i] !== (16'h0000 | i+1))
-        //         $display("FAIL thread %0d: expected %h got %h", 
-        //               i, 16'h0000 | i, data_out[i]);
-        //     else
-        //         $display("PASS thread %0d: got %h", i, data_out[i]);
-        // end
-
         //test 7: cause conflicts on multiple banks
         for(integer i = 0; i < 4; i++) begin
             addr[i] = {8'b0, i[3 : 0], 4'b0001};
@@ -172,7 +169,7 @@ end
         mem_req = 0;
         repeat(8)@(posedge clk);
 
-        //test 8: read this data
+        //test 9: multiple broadcasts
         for(integer i = 0; i < 4; i++) begin
             addr[i] = {8'b0, 4'b0000, 4'b0001}; //read bank 1(read the same register)
         end 
@@ -182,14 +179,37 @@ end
         for(integer i = 8; i < 12; i++) begin
             addr[i] = {8'b0, 4'b0000, 4'b0011}; //read bank 3(read the same register)
         end
-        for(integer i = 12; i < 16; i++) begin
-            addr[i] = {8'b0, 4'b0000, 4'b0100}; //read bank 4
+        for(integer i = 12; i < 15; i++) begin
+            if(i != 13)
+            addr[i] = {8'b0, i[3 : 0], 4'b0100}; //read bank 4
         end 
+            addr[15] = 16'h0003; //broadcast at  different locations
+            addr[13] = 16'h0003;
         @(posedge clk);
         mem_req = 1;
         repeat(2)@(posedge clk);
         mem_req = 0;
         repeat(6)@(posedge clk);
+
+        //test 10: a broadcasting bank has a conflict too
+        for(integer i = 0; i < 4; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0001}; //read bank 1(read the same register)
+        end 
+        for(integer i = 4; i < 8; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0010}; //read bank 2(read the same register)
+        end 
+        for(integer i = 8; i < 12; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0011}; //read bank 3(read the same register)
+        end
+        for(integer i = 12; i < 15; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0100}; //read bank 4
+        end 
+            addr[15] = 16'h00E4; // conflict in bank 4(read register 14)
+        @(posedge clk);
+        mem_req = 1;
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(20)@(posedge clk);
 
         
         $finish;
