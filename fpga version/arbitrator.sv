a4f825bd2e3062bedb13b64bd8f695ece92e14ef
Om <omgupta2806@gmail.com>
2026-06-30 15:50:58 +0530

    multidriver fix

--- fpga version/arbitrator.sv
+++ fpga version/arbitrator.sv
@@ -1,163 +1,217 @@
-module arbitrator #(
-    parameter BANKS      = 16,
-    parameter DW         = 16,
-    parameter NUMBER_OF_THREADS  = 16, //systolic grid size 
-    parameter NUMBER_OF_WARPS = 4,
-    parameter ADDR_DEPTH = 16
-) (
-    input logic clk,
-    input logic reset, 
-    input logic matmul,
-    input logic mem_write,
-    input logic mem_req,
-    input logic request_type, //eg 0 for lw and 1 sw
-    input logic [$clog2(NUMBER_OF_WARPS) - 1 : 0] warp_id_from_ws,//for multi-warp
-    input logic [NUMBER_OF_THREADS - 1 : 0]       active_mask,
-    input logic [ADDR_DEPTH - 1 : 0]              addr[0 : NUMBER_OF_THREADS - 1], // receive te whole address ad decode it here 
-    input  logic [DW - 1 : 0]                     data_in[0 : NUMBER_OF_THREADS - 1],
-    output logic [DW -  1 : 0]                    data_out[0 : NUMBER_OF_THREADS - 1], 
-    output logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]rf_to_access,//a mux at top level can allow stalled rf access
-    output logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]warp_id_to_ws, 
-    output logic stall
-);
-    //i only need to save the addresses, warp number and request type
-    //rf access can be done using the warp number and the address generate by the alu 
-    //because that warp is stalled anyways so i can access them using the same port
-    logic[ADDR_DEPTH - 1 : 0] saved_addr [0 : NUMBER_OF_THREADS - 1];
-    logic[3 : 0] addr_depth [0 : NUMBER_OF_THREADS - 1];
-    logic[3 : 0] addr_bank  [0 : NUMBER_OF_THREADS - 1];
-    logic[DW - 1 : 0] read_data [0 : NUMBER_OF_THREADS - 1];
+module tb_memory_arbitrator;
+    parameter BANKS      = 16;
+    parameter DW         = 16;
+    parameter NUMBER_OF_THREADS  = 16; //systolic grid size 
+    parameter ADDR_DEPTH = 16;
+    parameter NUMBER_OF_WARPS = 4;
 
-    logic[NUMBER_OF_THREADS - 1 : 0] bank_request [0 : NUMBER_OF_THREADS - 1];
-    logic[3 : 0] grant [0 : NUMBER_OF_THREADS - 1];
+    logic clk;
+    logic reset; 
+    logic matmul;
+    logic mem_write;
+    logic mem_req;
+    logic request_type;
+    logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]warp_id_from_ws;
+    logic[ADDR_DEPTH - 1 : 0]              addr[0 : NUMBER_OF_THREADS - 1]; // receive te whole address ad decode it here 
+    logic[DW - 1 : 0]                      data_in[0 : NUMBER_OF_THREADS - 1];
+    logic[DW -  1 : 0]                     data_out[0 : NUMBER_OF_THREADS - 1];  
+    logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]rf_to_access;
+    logic [$clog2(NUMBER_OF_WARPS) - 1 : 0]warp_id_to_ws;  
+    logic[NUMBER_OF_THREADS - 1 : 0]       active_mask;
+    logic stall;
 
-    logic grant_found;
-    logic broadcast_found;
-    logic grant_mask[0 : NUMBER_OF_THREADS - 1];
-    logic[3 : 0] broadcast [0 : NUMBER_OF_THREADS - 1];
-    logic[3 : 0] broadcast_bank [0 : NUMBER_OF_THREADS - 1];
-    logic[3 : 0] bank_to_read [0 : NUMBER_OF_THREADS - 1];
-    logic is_broadcast[0 : NUMBER_OF_THREADS - 1];
+    arbitrator dut (.*);
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
 
-    logic any_pending;
-    logic mem_req_latched;
-    always_comb begin 
-        if(reset) begin
-            for (integer i = 0; i < BANKS; i++) begin
-                broadcast[i] = 0;
-                broadcast_bank[i] = 0; 
-                stall = 0;
-                is_broadcast[i] = 0;
-            end            
-        end
-        if(mem_req) begin
-            for (integer i = 0; i < BANKS; i++) begin
-                addr_bank[i]  = addr[i][3 : 0]; //bank address
-                addr_depth[i] = addr[i][7 : 4]; //depth address 
-            end
-            for (integer i = 0; i < BANKS; i++) begin
-                for (integer j = 0; j < BANKS; j++) begin
-                    if((i != j) && (addr_bank[i] == addr_bank[j]) && (addr_depth[i] == addr_depth[j]) && !mem_write) begin
-                        broadcast[i] = i;
-                        broadcast_bank[i] = addr_bank[i];
-                        is_broadcast[i] = 1;
-                    end
-                end       
-            end
-        end
-        any_pending = '0;
+    initial begin
+        reset = 1;
+        repeat(3)@(posedge clk);
+        reset = 0; 
+        active_mask = 16'hFFFF;
+
+        //test 1 : write some data into the banks(cause conflicts too)
+        @(posedge clk);
         for (integer i = 0; i < BANKS; i++) begin
-            any_pending = any_pending | (|bank_request[i]); 
-        end
-        stall = any_pending;
-        if(!stall) begin
-            for(integer i = 0; i < BANKS; i++) begin
-                broadcast[i] = 0;
-                broadcast_bank[i] = 0;   
-                is_broadcast[i] = 0;
-            end     
-        end
-    end
-//i have to generate grants to individual threads to write in a specific bank
-    always_ff @(posedge clk) begin
-        if(reset) begin
-            for (integer i = 0; i < BANKS; i++) begin
-                grant[i]    <= 0;
-                bank_request[i] <= 'b0;
-                grant_mask[i] <= 0;
-                bank_to_read[i] <= 0;
-            end
-            mem_req_latched <= 0;
-        end
-        else if(mem_req) begin
-            for (integer i = 0; i < BANKS; i++) begin
-                for (integer j = 0; j < BANKS; j++) begin
-                    bank_request[i][j] <= (addr_bank[j] == i); //which threads are requesting which bank
-                    bank_to_read[i] <= addr_bank[i];
-                    mem_req_latched <= 1;
-                end
-            end            
-        end
-        else begin
-            mem_req_latched <= 0;
-            for (integer i = 0; i < BANKS; i++) begin
-                grant_found = 0;
-                for(integer j = 0; j < BANKS; j++) begin
-                    if(bank_request[i][j]) begin
-                        if(is_broadcast[j]) begin
-                            bank_request[i][j] <= 0;
-                            grant[i]      <= j;
-                            grant_mask[i] <= 1'b1;
-                            grant_found = 1;
-                        end
-                        else if(!grant_found && !is_broadcast[j]) begin
-                            grant[i]   <= j; //jth thread wants to access the first bank and so on
-                            grant_found = 1;
-                            bank_request[i][j] <= 0;
-                            grant_mask[i] <= 1'b1;
-                        end
-                    end
-                    else if(bank_request[i] == 0 && is_broadcast[i] == 0) begin
-                        grant[i] <= 0;
-                        grant_mask[i] <= 0;
-                    end
-                end              
-            end                       
-        end
-    end
-
-    genvar i;
-    generate
-        for (i = 0; i < BANKS; i++) begin   
-            memory_bank mem_bank (
-                .bank_en(addr_bank[grant[i]] == i[3 : 0] && active_mask[grant[i]] == 1 && grant_mask[i]), //comparison to check if bank number equals the address(this is wrong tho)
-                .clk(clk),
-                .reset(reset),
-                .matmul(matmul),
-                .mem_write(mem_write),
-                .addr_depth(addr_depth[grant[i]]),
-                .data_in(data_in[grant[i]]),
-                .data_out(read_data[i]) //idk why it isnt allowing me to use grant[i] here
-            );
-        end
-    endgenerate
-
-    always_comb begin 
-        //this exists only for debugging purposes
-        // for (integer i = 0; i < BANKS; i++) begin
-        //     data_out[i] = 'b0;
-        // end
-        for (integer i = 0; i < BANKS; i++) begin
-            if(grant_mask[i])begin
-                data_out[grant[i]] = read_data[i];
-            end           
-        end
-        //bank_request can be used for mixed request types
-        for (integer i = 0; i < BANKS; i++) begin
-            if(is_broadcast[i] && !mem_req_latched) begin
-                data_out[broadcast[i]] = read_data[broadcast_bank[i]];
-                is_broadcast[i] = 0;
+            if(i != 1 && i != 3 && i != 2) begin
+                addr[i] = i[15 : 0];
+                data_in[i] = i+1;
             end
         end
+        
+        addr[1] = 16'h0021;
+        data_in[1] = 16'h0001;
+        addr[3] = 16'h0031;
+        data_in[3] = 16'h0003;
+        addr[2] = 16'h0041;
+        data_in[2] = 16'h0002;
+        mem_write = 1;
+        @(posedge clk);
+        mem_req = 1;
+        repeat(2)@(posedge clk);
+        mem_write = 1;
+        mem_req = 0;
+        repeat(4)@(posedge clk);
+        mem_write = 0;
+
+        //test 2: read the previously written data
+        mem_req = 1;
+        @(posedge clk);
+        for (integer i = 0; i < BANKS; i++) begin
+            if(i != 1 && i != 3 && i != 2) begin
+                addr[i] = i[15 : 0];
+            end
+        end
+        addr[1] = 16'h0031; //read by thread 1 written by thread 3
+        addr[3] = 16'h0021; //read by thread 3 written by thread 1
+        addr[2] = 16'h0041;
+        @(posedge clk);
+        mem_req = 0;
+        repeat(3)@(posedge clk);
+
+        //test 3: Now cause no conflicts but first write some data
+        @(posedge clk);
+        for (integer i = 0; i < BANKS; i++) begin
+            addr[i] = i[15 : 0];
+            data_in[i] = i*2+1;
+        end 
+        @(posedge clk);
+        mem_write = 1;  
+        mem_req = 1; 
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(3)@(posedge clk);
+        mem_write = 0;
+        mem_req = 0;
+        @(posedge clk);
+
+        //test 4: Now read that data
+        mem_req = 1;
+        for (integer i = 0; i < BANKS; i++) begin
+            if(i != 2 && i != 4) begin
+                addr[i] = i[15 : 0];
+            end
+        end    
+        addr[2] = 16'h0004; //written by thread 4 read by thread 2
+        addr[4] = 16'h0002; //written by thread 2 read by thread 4
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        @(posedge clk);     
+
+        //test 5: send all requests to bank 1
+        for(integer i = 0; i < BANKS; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b1};
+            data_in[i] = i+1;
+        end   
+        @(posedge clk);
+        mem_req = 1;
+        mem_write = 1;
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(17)@(posedge clk);
+        mem_write = 0;
+
+
+        //test 6 read that data
+        mem_req  = 1;
+        for (integer i = 0; i < BANKS; i++) begin
+            addr[i] = {8'b0, i[3:0], 4'b1};  // same addresses
+        end
+        @(posedge clk);
+        mem_req = 0;
+        repeat(17) @(posedge clk);
+
+
+        //test 7: cause conflicts on multiple banks
+        for(integer i = 0; i < 4; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0001};
+            data_in[i] = i+1;
+        end 
+        for(integer i = 4; i < 8; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0010};
+            data_in[i] = i+1;
+        end 
+        for(integer i = 8; i < 12; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0011};
+            data_in[i] = i+1;
+        end
+        for(integer i = 12; i < 16; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0100};
+            data_in[i] = i+1;
+        end 
+        @(posedge clk);
+        mem_req = 1;
+        mem_write = 1;
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(6)@(posedge clk);
+        mem_write = 0;    
+        @(posedge clk);   
+
+        //test 8: read this data
+        for(integer i = 0; i < 4; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0001}; //read bank 1(read the same register)
+        end 
+        for(integer i = 4; i < 8; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0010}; //read bank 2
+        end 
+        for(integer i = 8; i < 12; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0011}; //read bank 3
+        end
+        for(integer i = 12; i < 16; i++) begin
+            addr[i] = {8'b0, i[3 : 0], 4'b0100}; //read bank 4
+        end 
+        @(posedge clk);
+        mem_req = 1;
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(8)@(posedge clk);
+
+        //test 9: multiple broadcasts
+        for(integer i = 0; i < 4; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0001}; //read bank 1(read the same register)
+        end 
+        for(integer i = 4; i < 8; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0010}; //read bank 2(read the same register)
+        end 
+        for(integer i = 8; i < 12; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0011}; //read bank 3(read the same register)
+        end
+        for(integer i = 12; i < 15; i++) begin
+            if(i != 13)
+            addr[i] = {8'b0, i[3 : 0], 4'b0100}; //read bank 4
+        end 
+            addr[15] = 16'h0003; //broadcast at  different locations
+            addr[13] = 16'h0003;
+        @(posedge clk);
+        mem_req = 1;
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(6)@(posedge clk);
+
+        //test 10: a broadcasting bank has a conflict too
+        for(integer i = 0; i < 4; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0001}; //read bank 1(read the same register)
+        end 
+        for(integer i = 4; i < 8; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0010}; //read bank 2(read the same register)
+        end 
+        for(integer i = 8; i < 12; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0011}; //read bank 3(read the same register)
+        end
+        for(integer i = 12; i < 15; i++) begin
+            addr[i] = {8'b0, 4'b0000, 4'b0100}; //read bank 4
+        end 
+            addr[15] = 16'h00E4; // conflict in bank 4(read register 14)
+        @(posedge clk);
+        mem_req = 1;
+        repeat(2)@(posedge clk);
+        mem_req = 0;
+        repeat(20)@(posedge clk);
+
+        
+        $finish;
     end
 endmodule
