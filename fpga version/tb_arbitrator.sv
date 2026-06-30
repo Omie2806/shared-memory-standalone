647bcfba8abe09441948f7d8738442ecc891217e
Om <omgupta2806@gmail.com>
2026-06-30 11:39:52 +0530

    Add testbench for tb_arbitrator_fsm module
    This testbench simulates the tb_arbitrator_fsm module, providing a clock signal and a sequence of test numbers to validate its functionality.

---
+++ fpga version/tb_arbitrator.sv
@@ -0,0 +1,43 @@
+module tb_fsm_test;
+logic clk;
+logic reset;
+logic[3 : 0] test_number;
+logic done;
+logic error;
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+tb_arbitrator_fsm fsm_inst (.*);
+
+initial begin
+    reset = 1;
+    repeat(2)@(posedge clk);
+    reset = 0;
+    test_number = 4'b0000;
+   repeat(10)@(posedge clk);
+   test_number = 4'b0001;
+   repeat(10)@(posedge clk);
+   test_number = 4'b0010;
+   repeat(9)@(posedge clk);
+   test_number = 4'b0011;
+   repeat(9)@(posedge clk);
+   test_number = 4'b0100;
+   repeat(22)@(posedge clk);   
+   test_number = 4'b0101;
+   repeat(22)@(posedge clk);  
+   test_number = 4'b0110;
+   repeat(12)@(posedge clk);
+   test_number = 4'b0111;
+   repeat(12)@(posedge clk);
+   test_number = 4'b1000;
+   repeat(12)@(posedge clk);
+   test_number = 4'b1001;
+   repeat(12)@(posedge clk);
+   $finish; 
+end
+
+
+endmodule
