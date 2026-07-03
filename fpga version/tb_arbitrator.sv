766475da2a3765ebf15b8bc2cae9182813527634
Om <omgupta2806@gmail.com>
2026-07-03 13:08:50 +0530

    fpga top

--- fpga version/tb_arbitrator.sv
+++ fpga version/tb_arbitrator.sv
@@ -1,46 +1,47 @@
-module tb_fsm_test;
-logic clk;
-logic reset;
-logic[3 : 0] test_number;
-logic mem_req_out;
-logic mem_write_out;
-logic data_in_out;
-logic done;
-logic error;
+module tb_fsm_test (
+    input logic clk,
+    input logic reset,
+    input logic[3 : 0] test_number,
+    output logic mem_req_out,
+    output logic mem_write_out,
+    output logic data_in_out,
+    output logic done,
+    output logic error
+);
 
-initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
-end
+//initial begin
+//    clk = 0;
+//    forever #5 clk = ~clk;
+//end
 
 tb_arbitrator_fsm fsm_inst (.*);
 
-initial begin
-    reset = 1;
-    repeat(2)@(posedge clk);
-    reset = 0;
-    test_number = 4'b0000;
-   repeat(10)@(posedge clk);
-   test_number = 4'b0001;
-   repeat(10)@(posedge clk);
-   test_number = 4'b0010;
-   repeat(9)@(posedge clk);
-   test_number = 4'b0011;
-   repeat(9)@(posedge clk);
-   test_number = 4'b0100;
-   repeat(22)@(posedge clk);   
-   test_number = 4'b0101;
-   repeat(22)@(posedge clk);  
-   test_number = 4'b0110;
-   repeat(12)@(posedge clk);
-   test_number = 4'b0111;
-   repeat(12)@(posedge clk);
-   test_number = 4'b1000;
-   repeat(12)@(posedge clk);
-   test_number = 4'b1001;
-   repeat(12)@(posedge clk);
-   $finish; 
-end
+//initial begin
+//    reset = 1;
+//    repeat(2)@(posedge clk);
+//    reset = 0;
+//    test_number = 4'b0000;
+//   repeat(10)@(posedge clk);
+//   test_number = 4'b0001;
+//   repeat(10)@(posedge clk);
+//   test_number = 4'b0010;
+//   repeat(9)@(posedge clk);
+//   test_number = 4'b0011;
+//   repeat(9)@(posedge clk);
+//   test_number = 4'b0100;
+//   repeat(22)@(posedge clk);   
+//   test_number = 4'b0101;
+//   repeat(22)@(posedge clk);  
+//   test_number = 4'b0110;
+//   repeat(12)@(posedge clk);
+//   test_number = 4'b0111;
+//   repeat(12)@(posedge clk);
+//   test_number = 4'b1000;
+//   repeat(12)@(posedge clk);
+//   test_number = 4'b1001;
+//   repeat(12)@(posedge clk);
+//   $finish; 
+//end
 
 
 endmodule
